5e72228f216eb9b647a8f9ab643d5e28c4720988
Sone <sthaya@gmail.com>
2019-08-27 21:49:08 -0700

    worked on Recipe component added details and recipe url buttons and images

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { recipes } from './tempList';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
+export class App extends Component {
+  state = {
+    recipes: recipes,
+    url:
+      'https://www.food2fork.com/api/search?key=1f48e0c0def3bbd3ce97533a20a2f0da'
+  };
 
-function App() {
-  return <React.Fragment>
-    <RecipeList />
-    <RecipeDetails />
-  </React.Fragment>;
+  // async getRecipes() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   this.getRecipes();
+  // }
+
+  render() {
+    // console.log(this.state.recipes);
+    return (
+      <React.Fragment>
+        <RecipeList recipes={this.state.recipes} />
+        <RecipeDetails />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
